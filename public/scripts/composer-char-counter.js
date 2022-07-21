4df556b6f5275f2fcff88b85baa265cde807868f
Robert Servado <servadorobertgmail.com>
2022-07-21 13:42:06 -0400

    Modified composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     const sentenceLength = $(this).val().length;
 
     // Traversing the DOM: this = #tweet-text, sibling of it is another div with #tweet-container and within it is where we want to go which is the children with ".counter"
-    const counter = $(this).siblings("#tweet-container").children(".counter");
+    const counter = $(this).siblings("#tweetDisplay").children(".counter");
 
     const remainingCharacters = tweetLimit - sentenceLength;
     counter.val(remainingCharacters);
