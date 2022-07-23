3e80123117a090d4afa6d43194ef00a8df34d28d
Robert Servado <servadorobertgmail.com>
2022-07-23 19:27:25 -0400

    Modified client.js to show the get route using ajax and jQuery to load new tweets and implement the timing sytem for each tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,50 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// DYNAMIC HTML FUNCTION - WEEK 9
+const createTweetElement = function (userKey) {
+  const timeSince = timeago.format(userKey.created_at);
+  let $tweet = $(`
+    <article>
+      <header>
+        <span class="user"><img class="user" src="${userKey.user.avatars}" alt="Face Logo Image"/>
+        <p class="handle">${userKey.user.name}</p></span>
+        <p class="handle">${userKey.user.handle}</p>
+     </header>
+        <p class="handle">${userKey.content.text}</p>
+      <footer>
+        <span>${timeSince}</span>
+        <span class ="buttons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </span>
+      </footer>
+    </article>`);
+  return $tweet;
+};
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+const renderTweets = function (users) {
+  for (let userTweet of users) {
+    $("#tweets-container").prepend(createTweetElement(userTweet));
+  }
+};
+
+const loadTweet = function () {
+  // Can be written in the short hand in the future refactor by using the shorthand method and using then and catch. Also separate it in an another function
+  $.ajax({
+    type: "GET",
+    url: "/tweets",
+    dataType: "json",
+    success: (data) => {
+      console.log("This request succeeded and here's the data: ", data);
+      $("#tweets-container").empty();
+      renderTweets(data);
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
+    error: (error) => {
+      console.log("This request failed and the was the error: ", error);
     },
-    created_at: 1658376396392,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1658462796392,
-  },
-];
+  });
+};
 
 $(document).ready(function () {
   $("form").on("submit", function (event) {
@@ -45,37 +63,7 @@ $(document).ready(function () {
       data: tweetData,
       success: (data) => {
         console.log("This request succeeded and here's the data: ", data);
-
-        const renderTweets = function (users) {
-          for (let userTweet of users) {
-            $("#tweets-container").append(createTweetElement(userTweet));
-          }
-        };
-
-        const createTweetElement = function (userKey) {
-          let $tweet = $(`
-            <article>
-              <header>
-                <span class="user"><img class="user" src="${userKey.user.avatars}" alt="Face Logo Image">
-                <p>${userKey.user.name}</p></span>
-                <p class="handle">${userKey.user.handle}</p>
-             </header>
-                <p>${userKey.content.text}</p>
-        
-              <footer>
-                <span>5 days ago</span>
-                <span class ="buttons">
-                  <i class="fas fa-flag"></i>
-                  <i class="fas fa-retweet"></i>
-                  <i class="fas fa-heart"></i>
-                </span>
-              </footer>
-            </article>`);
-
-          return $tweet;
-        };
-
-        renderTweets(data);
+        loadTweet();
       },
       error: (error) => {
         console.log("This request failed and the was the error: ", error);
@@ -107,7 +95,6 @@ $(document).ready(function () {
 //         <p class="handle">${data.user.handle}</p>
 //      </header>
 //         <p>${data.content.text}</p>
-
 //       <footer>
 //         <span> 5 days ago</span>
 //         <span class ="buttons">
