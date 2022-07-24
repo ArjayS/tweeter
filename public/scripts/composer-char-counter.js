922622d6ebb7c34f11eec876640f18331e93bcb4
Robert Servado <servadorobertgmail.com>
2022-07-24 15:22:16 -0400

    Modified comoposer-char-counter.js to clean up the code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,15 @@
-$(document).ready(function () {
-  // --- our code goes here ---
-  // console.log("Hello World");
-  // $(".new-tweet").blur(function () {
-  //   console.log("Hi");
-  // });
-  // console.log("#tweet-");
-  $("#tweet-text").on("input", function () {
+$(() => {
+  $("#tweet-text").on("input", function() {
     const tweetLimit = 140;
     const sentenceLength = $(this).val().length;
 
-    // Traversing the DOM: this = #tweet-text, sibling of it is another div with #tweet-container and within it is where we want to go which is the children with ".counter"
+    // Traversing the DOM
     const counter = $(this).siblings("#tweetDisplay").children(".counter");
 
     const remainingCharacters = tweetLimit - sentenceLength;
     counter.val(remainingCharacters);
 
+    //Character limit check
     if (remainingCharacters < 0) {
       $(".counter").addClass("counterLimit");
     } else {
