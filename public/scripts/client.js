3d9d5f6f7c8455330eaaddb08e6967de2076b24c
Robert Servado <servadorobertgmail.com>
2022-07-23 23:55:30 -0400

    Modified client.js to add escape feature so that no javascript code can manipulate on the input of the tweeter app after submit is hit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const createTweetElement = function (userKey) {
   const timeSince = timeago.format(userKey.created_at);
+  const safeHTML = escape(userKey.content.text);
   let $tweet = $(`
     <article>
       <header>
@@ -13,7 +20,7 @@ const createTweetElement = function (userKey) {
         <p class="name-handle">${userKey.user.name}</p></span>
         <p class="at-handle">${userKey.user.handle}</p>
      </header>
-        <p class="text-handle">${userKey.content.text}</p>
+        <p class="text-handle">${safeHTML}</p>
       <footer>
         <span>${timeSince}</span>
         <span class ="buttons">
@@ -27,6 +34,11 @@ const createTweetElement = function (userKey) {
 };
 
 const renderTweets = function (users) {
+  // // Another way to loop through the object using map and applying each of the data to prepend
+  // users.map((userTweet) => {
+  //   $("#tweets-container").prepend(createTweetElement(userTweet));
+  // });
+
   for (let userTweet of users) {
     $("#tweets-container").prepend(createTweetElement(userTweet));
   }
@@ -81,76 +93,3 @@ $(document).ready(function () {
     }
   });
 });
-
-// // I WENT ABOVE AND BEYOND WHEN STATING WEEK 9 - BUT THIS IS MOST OF THE PROJECT BELOW. WILL FOLLOW THE DYNAMIC HTML SECTION OF WEEK 9.
-
-// const loadTweet = function () {
-//   // Can be written in the short hand in the future refactor by using the shorthand method and using then and catch. Also separate it in an another function
-//   $.ajax({
-//     type: "GET",
-//     url: "/tweets",
-//     dataType: "json",
-//     success: (data) => {
-//       console.log("This request succeeded and here's the data: ", data);
-//       $("#tweetContainer").empty();
-
-//       const timeSince = Math.floor(Date.now() / 1000);
-
-//       const createNewTweet = (data) => {
-//         const $tweet = $(`
-//     <article>
-//       <header>
-//         <span class="user"><img class="user" src="${data.user.avatars}" alt="Face Logo Image">
-//         <p>${data.user.name}</p></span>
-//         <p class="handle">${data.user.handle}</p>
-//      </header>
-//         <p>${data.content.text}</p>
-//       <footer>
-//         <span> 5 days ago</span>
-//         <span class ="buttons">
-//           <i class="fas fa-flag"></i>
-//           <i class="fas fa-retweet"></i>
-//           <i class="fas fa-heart"></i>
-//         </span>
-//       </footer>
-//     </article>`);
-
-//         return $tweet;
-//       };
-
-//       // data.map((tweet) => {
-//       //   $("#tweetContainer").prepend(createNewTweet(tweet));
-//       // });
-
-//       for (let tweet of data) {
-//         $("#tweetContainer").prepend(createNewTweet(tweet));
-//       }
-//     },
-//     error: (error) => {
-//       console.log("This request failed and the was the error: ", error);
-//     },
-//   });
-// };
-
-// $(() => {
-//   $("form").on("submit", function (event) {
-//     event.preventDefault();
-
-//     // Creating a tweet data variable
-//     const tweetData = $(this).serialize();
-
-//     // Can be written in the short hand in the future refactor by using the shorthand method and using then and catch. Also separate it in an another function.
-//     $.ajax({
-//       type: "POST",
-//       url: "/tweets",
-//       data: tweetData,
-//       success: (data) => {
-//         console.log("This request succeeded and here's the data: ", data);
-//         loadTweet();
-//       },
-//       error: (error) => {
-//         console.log("This request failed and the was the error: ", error);
-//       },
-//     });
-//   });
-// });
